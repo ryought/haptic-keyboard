43bdbd2d69434448f6262cc5db63bd74414907cb
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-05-02 13:23:57 +0900

    Initial Commit

---
+++ haptic-keyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  haptic-keyboard
+//
+//  Created by Ryo Nakabayashi on 2019/05/02.
+//  Copyright © 2019 Ryo Nakabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
